8a02ca244c9f883aecdd55715b7c3849a7a84027
YlnSs <114404266+YlnSs@users.noreply.github.com>
2024-03-13 01:35:11 +0300

    Update md5cracker.php

--- md5cracker.php
+++ md5cracker.php
@@ -5,7 +5,7 @@
 <body>
 <h1>MD5 hash-code cracker</h1>
 <p>This application takes an MD5 hash
-of a two-character lower case string and 
+of a four-character lower case string and 
 attempts to hash all two-character combinations
 to determine the original two characters</p>
 <pre>
